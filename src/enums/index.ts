d6d83e83abc79e507a37893794860a064778b668
Kunal Garg <kunal@usebracket.com>
2023-03-13 16:43:47 -0400

    feat: add Host, SyncStrategy, MutationType, SecurityPolicy, SyncDirection, TableOptions

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,1 +1,33 @@
 export * from './intersectionFieldType';
+
+export enum Host {
+  single_tenant = 'single-tenant',
+  multi_tenant = 'multi-tenant',
+}
+
+export enum SyncStrategy {
+  iterative = 'iterative',
+  asynchronous = 'asynchronous',
+  stream = 'stream',
+}
+
+export enum MutationType {
+  update = 'UPDATE',
+  upsert = 'UPSERT',
+}
+
+export enum SecurityPolicy {
+  DYNAMIC_IP_ADDRESS = 'dynamic',
+  STATIC_IP_ADDRESS = 'static',
+}
+
+export enum SyncDirection {
+  FROM_PRIMARY_TO_SECONDARY = '1',
+  FROM_SECONDARY_TO_PRIMARY = '2',
+  BIDIRECTIONAL = '3',
+}
+
+export enum TableOptions {
+  CREATE_TABLE = '1',
+  USE_EXISTING_TABLE = '2',
+}
