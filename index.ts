461e8a78bc0d50d5fde66235a1bc4e9af8b5b0cd
Kunal Garg <kunal@usebracket.com>
2023-03-14 11:10:21 -0400

    fix: exporting all enums in src/index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,19 @@
-import { IntersectionFieldType } from './src/enums';
+import {
+  IntersectionFieldType,
+  MutationType,
+  Host,
+  SyncDirection,
+  SyncStrategy,
+  TableOptions,
+  SecurityPolicy,
+} from './src/enums';
 import { AirtableTypes } from './src/types/data-sources/AirtableTypes';
 
-export { IntersectionFieldType, AirtableTypes };
+export {
+  IntersectionFieldType, AirtableTypes, MutationType,
+  Host,
+  SyncDirection,
+  SyncStrategy,
+  TableOptions,
+  SecurityPolicy,
+};
