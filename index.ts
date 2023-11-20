6e6b90e9a3be25a343e47782651600c795daa0fb
Kunal Garg <kunal@usebracket.com>
2023-11-20 13:08:10 -0500

    fixing type imports

--- index.ts
+++ index.ts
@@ -1,9 +1,3 @@
-import {
-  AirtableTypes,
-} from './src/types/data-sources/AirtableTypes';
-
 export * from './src/enums';
 export * from './src/constants';
-export {
-  AirtableTypes,
-};
+export * from './src/types/data-sources';
---
+++ src/types/data-sources/index.ts
@@ -0,0 +1,4 @@
+import { AirtableTypes } from './AirtableTypes';
+import { NotionTypes } from './NotionTypes';
+
+export { AirtableTypes, NotionTypes };
